7e6b0bdd91d9a77106438bfb461b3ec41f5acdce
Martin Dilling-Hansen <martind@ruc.dk>
2020-09-18 11:49:38 +0200

    Fixed a few issues

--- 009-php/install.sh
+++ 009-php/install.sh
@@ -15,6 +15,6 @@ sudo apt update -y
 
 # Install apt packages
 sudo apt install -y php7.4 php7.4-common php7.4-cli php7.4-fpm
-sudo apt install -y php7.4-curl php7.4-json php7.4-gd php7.4-mbstring php7.4-intl php7.4-bcmath php7.4-zip
+sudo apt install -y php7.4-curl php7.4-json php7.4-gd php7.4-mbstring php7.4-intl php7.4-bcmath php7.4-zip php7.4-xml
 
 finished
--- 011-homestead/install.sh
+++ 011-homestead/install.sh
@@ -7,15 +7,24 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Install homestead
 installing "laravel/homestead box"
-vagrant box add laravel/homestead --provider virtualbox
+if [ 0 = "$(vagrant box list | grep -o "laravel/homestead" | wc -l)" ]; then
+  vagrant box add laravel/homestead --provider virtualbox
+else
+  echo "laravel/homestead already added."
+fi
 
 cloning "laravel/homestead"
-git clone https://github.com/laravel/homestead.git ~/Homestead
-
-updating "homestead"
-pushd ~/Homestead >/dev/null 2>&1
-git checkout release
-bash init.sh
-popd >/dev/null 2>&1
+homestead_dir="${HOME}/Homestead"
+homestead_branch="20.04"
+if [ -d "${homestead_dir}" ]; then
+  echo "${homestead_dir} already exist. If it needs to be updated, do so manually."
+else
+  git clone https://github.com/laravel/homestead.git ${homestead_dir}
+  updating "homestead"
+  pushd "${homestead_dir}" >/dev/null 2>&1
+  git checkout "${homestead_branch}"
+  bash init.sh
+  popd >/dev/null 2>&1
+fi
 
 finished
